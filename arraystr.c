c039fea08938907d427d07ba47b5101802de4a1e
FUYUHSUAN <ramy1231863@gmail.com>
2019-12-27 09:25:52 +0800

    add arraystr.c

--- arraystr.c
+++ arraystr.c
@@ -1,18 +1,20 @@
 #include<stdio.h>
-#define SIZE 5
+#include <string.h>
+#define SIZE 1000
 int main(void)
 {
     int i, k;
     char temp;
     char str[SIZE];
     printf("Please enter your words:");
-    scanf("%s",&str);
+    scanf("%s", str);
+    int len = strlen(str);
 
-    for(i=0;i<=SIZE/2-1;i++){
+    for(i=0;i<=len/2-1;i++){
         temp=str[i];
-        str[i] = str[SIZE-1-i] ;
-        str[SIZE-1-i] = temp  ;
-       k = SIZE-1-i ;
+        str[i] = str[len-1-i] ;
+        str[len-1-i] = temp  ;
+       //k = SIZE-1-i ;
         //printf("%d\n",k);
     }
 
